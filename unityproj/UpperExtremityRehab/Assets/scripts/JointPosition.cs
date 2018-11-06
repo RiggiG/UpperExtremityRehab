ce788f513eda0264e0aeccd662ad330f9abbe3c3
RiggiG <44820045+RiggiG@users.noreply.github.com>
2018-11-06 16:37:07 -0500

    initial commit
    unity proj, kinect examples

---
+++ unityproj/UpperExtremityRehab/Assets/scripts/JointPosition.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class JointPosition : MonoBehaviour 
+{
+    public Windows.Kinect.JointType _jointType;
+    public GameObject _bodySourceManager;
+    private BodySourceManager _bodyManager;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (_bodySourceManager == null)
+        {
+            return;
+        }
+
+        _bodyManager = _bodySourceManager.GetComponent<BodySourceManager>();
+        if (_bodyManager == null)
+        {
+            return;
+        }
+
+        Body[] data = _bodyManager.GetData();
+        if (data == null)
+        {
+            return;
+        }
+
+        // get the first tracked body...
+        foreach (var body in data)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+
+            if (body.IsTracked)
+            {
+               //this.gameObject.transform.position = new Vector3
+               // this.gameObject.transform.localPosition =  body.Joints[_jointType].Position;
+                var pos = body.Joints[_jointType].Position;
+               this.gameObject.transform.position = new Vector3(pos.X, pos.Y, pos.Z);
+                break;
+            }
+        }
+	}
+}
