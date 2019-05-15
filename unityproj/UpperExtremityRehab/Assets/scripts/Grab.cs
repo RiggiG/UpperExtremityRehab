b09175ceb239b5c911fda9126580773435c72573
RiggiG <44820045+RiggiG@users.noreply.github.com>
2019-05-15 19:49:46 -0400

    VR implementation complete, pouring all set

--- unityproj/UpperExtremityRehab/Assets/scripts/Grab.cs
+++ unityproj/UpperExtremityRehab/Assets/scripts/Grab.cs
@@ -1,24 +1,93 @@
 using UnityEngine;
 using System.Collections;
-using System;
+/**using System;
+using System.Collections.Generic;
+
+
 
 public class Grab : MonoBehaviour
 {
-	public GameObject _grabPosition;
+	//public GameObject _grabPosition;
+	private SteamVR_TrackedObject trackedObj;
 	
+	private GameObject collidingObject;
+	private GameObject objectInHand;
 	
-	void Start()
+	private SteamVR_Controller.Device Controller
 	{
-		
+		get { return SteamVR_Controller.Input((int)trackedObj.index);}
 	}
-	
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
 	
 	void Update()
 	{
-		
+		if (Controller.GetHairTriggerDown())
+		{
+			if (collidingObject && collidingObject.tag == "canGrab") {GrabObject();}
+		}
+		if (Controller.GetHairTriggerUp())
+		{
+			if (objectInHand) {ReleaseObject();}
+		}
 		
 	}
+	private void SetCollidingObject(Collider col)
+	{
+		if (collidingObject || !col.GetComponent<Rigidbody>())
+		{
+		return;
+		}
+		collidingObject = col.gameObject;
+	}
 	
+	public void OnTriggerEnter(Collider other)
+	{
+		SetCollidingObject(other);
+	}
+	
+	public void OnTriggerStay(Collider other)
+	{
+		SetCollidingObject(other);
+	}
+	
+	public void OnTriggerExit(Collider other)
+	{
+		if (!collidingObject)
+		{ return; }
+		collidingObject = null;
+	}
+	
+	private void GrabObject() 
+	{
+		objectInHand = collidingObject;
+		collidingObject = null;
+		var joint = AddFixedJoint();
+		joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+	}
+	
+	private FixedJoint AddFixedJoint()
+	{
+		FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+		fx.breakForce = 20000;
+		fx.breakTorque = 20000;
+		return fx;
+	}
+	
+	private void ReleaseObject()
+	{
+		if (GetComponent<FixedJoint>())
+		{
+			GetComponent<FixedJoint>().connectedBody = null;
+			Destroy(GetComponent<FixedJoint>());
+			objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+			objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+		}
+		objectInHand = null;
+	}
+/*from Kinect implementation	
 	GameObject GetClosestObject ()
     {
         GameObject[] gos;
@@ -39,4 +108,4 @@ public class Grab : MonoBehaviour
         return closest;
     }
 	
-}
+}**/
--- unityproj/UpperExtremityRehab/Assets/scripts/Pour.cs
+++ unityproj/UpperExtremityRehab/Assets/scripts/Pour.cs
@@ -16,19 +16,22 @@ public class Pour : MonoBehaviour
 	void Update ()
 	{
 		var rot = this.gameObject.transform.eulerAngles.z;
-		if (rot < -30) 
+		//Debug.Log("Carafe rotation: " + rot);
+		if (rot > 250 && rot < 290) 
 		{
+			//Debug.Log("Meets pour angle");
 			var closestMug = GetClosestMug();			
 			
-			if (_coffeeSource.gameObject.transform.localScale.y <= 0)
+			if (_coffeeSource.gameObject.transform.localScale.y <= 0.01)
 			{
+				Debug.Log("Carafe empty.");
 				return;
 			}
 			
-			_coffeeSource.gameObject.transform.localScale -= new Vector3(0,0.0001f,0);
-			if ((Math.Abs(closestMug.transform.position.z - _pourOrigin.transform.position.z) <= 0.025) && (Math.Abs(closestMug.transform.position.x - _pourOrigin.transform.position.x) <= 0.025))
+			_coffeeSource.gameObject.transform.localScale -= new Vector3(0,0.0005f,0);
+			if (Vector3.Distance(_pourOrigin.transform.position, closestMug.transform.position) < 0.3)
 			{
-				
+				Debug.Log("Attempting mug filling");
 				FillMug(closestMug);
 				
 			}
@@ -44,7 +47,7 @@ public class Pour : MonoBehaviour
         mugs = GameObject.FindGameObjectsWithTag("mug");
         GameObject closest = null;
         float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
+        Vector3 position = _pourOrigin.transform.position;
         foreach (GameObject mug in mugs)
         {
             Vector3 diff = mug.transform.position - position;
@@ -61,7 +64,7 @@ public class Pour : MonoBehaviour
 	void FillMug(GameObject toFill)
 	{
 		
-		toFill.gameObject.transform.GetChild(1).localScale += new Vector3(0,0.0001f,0);
+		toFill.gameObject.transform.GetChild(1).localScale += new Vector3(0,0.0005f,0);
 	}
 	
 }
